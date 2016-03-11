b2f469fbacb2064428bbf719a8d7f2e28397b7cb
KERIM KALAFALA <kerimkalafala@KERIMs-MacBook-Pro.local>
2016-03-10 20:35:36 -0500

    join examples.

--- lessons2.sql
+++ lessons2.sql
@@ -16,5 +16,12 @@ create table mytable2(id int(11) unsigned auto_increment primary key not null, u
 insert into mytable2(username, passwd, petname) values('user1', 'passwd1', 'pet1');
 insert into mytable2(username, passwd, petname) values('user2', 'passwd2', 'pet2');
 insert into mytable2(username, passwd, petname) values('user3', 'passwd3', 'pet3');
-insert into mytable2(username, passwd, petname) values('user4', 'passwd4', 'pet4');
+insert into mytable2(username, passwd, petname) values('user44', 'passwd4', 'pet4');
 select * from mytable2;
+
+select * from mytable cross join mytable2;  # cross-product (cartesian product) of mytable and mytable2
+select * from mytable left join mytable2 on mytable.username=mytable2.username; # Left-join, keep all entries in mytable
+select * from mytable right join mytable2 on mytable.username=mytable2.username; # Right-join, keep all entries in mytable2
+select * from mytable inner join mytable2 on mytable.username=mytable2.username; # inner-join, keep only entries where user-name matches
+
+select * from mytable inner join mytable2 on mytable.username=mytable2.username where mytable2.petname in ('pet2','pet3'); # inner-join, keep only entries where user-name matches
